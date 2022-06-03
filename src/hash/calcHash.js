e125c3fdcf5b566bfd6a9b92bb3c7cf36f561100
Alex Palx <alex.teanow@gmail.com>
2022-06-03 20:10:59 +0300

    feat: implement hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,26 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as url from 'url';
+import * as crypto from 'crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+    const filesFolderPath = path.join(__dirname, 'files');
+    const fileName = 'fileToCalculateHashFor.txt';
+
+    const filePath = path.join(filesFolderPath, fileName);
+
+    const hash = crypto.createHash('sha256');
+
+    const readStream = fs.createReadStream(filePath);
+
+    readStream.on('data', (data) => {
+        hash.update(data);
+    });
+
+    readStream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
 };
+
+calculateHash();
