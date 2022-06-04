fe80b25e5059891a983c266c0102f6191bd2b9e2
Alex Palx <alex.teanow@gmail.com>
2022-06-04 20:19:17 +0300

    feat: implement command line interface subtask

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,10 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const filteredKeys = Object.keys(process.env)
+        .filter(key => key.startsWith('RSS_'))
+        .map(key => `${key}=${process.env[key]}`)
+        .join('; ');
+    
+    console.log(filteredKeys);
 };
+
+parseEnv();
